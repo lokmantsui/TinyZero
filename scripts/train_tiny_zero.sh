7c65c1977c7a29d1b5f97b8792415d5c1361eb84
lmtsui <lokman@gridspace.com>
2025-01-28 23:50:31 +0000

    3b working

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -2,7 +2,7 @@ python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
 data.train_batch_size=128 \
-data.val_batch_size=32 \
+data.val_batch_size=128 \
 data.max_prompt_length=256 \
 data.max_response_length=1024 \
 actor_rollout_ref.model.path=$BASE_MODEL \
