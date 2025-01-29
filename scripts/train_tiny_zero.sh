65eeb50b2ba1540686d372016a02d0c0f93323da
lmtsui <lokman@gridspace.com>
2025-01-29 01:25:34 +0000

    still working

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -1,15 +1,15 @@
 python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
-data.train_batch_size=128 \
-data.val_batch_size=128 \
+data.train_batch_size=256 \
+data.val_batch_size=1312 \
 data.max_prompt_length=256 \
 data.max_response_length=1024 \
 actor_rollout_ref.model.path=$BASE_MODEL \
 actor_rollout_ref.actor.optim.lr=1e-6 \
 actor_rollout_ref.actor.ppo_mini_batch_size=128 \
 actor_rollout_ref.actor.ppo_micro_batch_size=4 \
-actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
+actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
 actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
 actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
